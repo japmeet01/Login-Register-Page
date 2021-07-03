9909327fb5f493439f470100968bcb823fb3a0c1
Japmeet Singh <japmeet01@gmail.com>
2021-07-03 19:26:41 +0530

    Updated the about page

--- about.php
+++ about.php
@@ -30,42 +30,30 @@
         </nav>
       </div>
     <div class="about">
-    <h1 class="h1">About-Us</h1>
-    <h2 class="h2">DETAILS OF MEMBERS:</h2>
-<div id="grid">
-    <div class="column">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSJOydfHJObk3KtBOwPeWfHZrQAuBBPZbs_L27Ym2v5snsfkA4rBvao5HPOCLe6sY8h4I&usqp=CAU"
-            alt="N-image" class="img">
-        <ul type="none"><br>
-            <li class="li">Name ==> <strong class="strong">NITIN SAINI</strong></li>
-            <li class="li">UID ==> <strong class="strong">20BCS1440</strong></li>
-            <li class="li">Section/Group ==> <strong class="strong">20ITA-19</strong></li>
-        </ul>
+    <h1 class="about_heading">Our Team</h1>
+    <div id="grid">
+    <div class="column1">
+      <img src="japmeet.jpeg" class="japmeet">
+      <p >Hi, I am Japmeet Singh.I love developing websites and through this quiz project I got to learn a lot of things starting from SQL databases to frontend designing.</p>
+      <h1 class="subheading">Contribution:</h1>
+      <p>Made the login and Register flow of the website using PHP and sql database. Also helped the team in designing the pages.</p>
     </div>
-    <div class="column">
-        <img src="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX9323154.jpg" alt="J-image" class="img">
-        <ul type="none"><br>
-            <li class="li">Name ==> <strong class="strong">JAPMEET SIR</strong></li>
-            <li class="li">UID ==> <strong class="strong">20BCS1442</strong></li>
-            <li class="li">Section/Group ==> <strong class="strong">20ITA-19</strong></li>
-        </ul>
+    <div class="column2">
+    <img src="japmeet.jpeg" class="japmeet">
+      <p >Hi, I am Japmeet Singh.I love developing websites and through this quiz project I got to learn a lot of things starting from SQL databases to frontend designing.</p>
+      <h1 class="subheading">Contribution:</h1>
+      <p>Made the login and Register flow of the website using PHP and sql database. Also helped the team in designing the pages.</p>
+        
     </div>
-    <div class="column">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbRT2Lcmj38gEGhfyxetRQKImZydz_yZ5AFdq9LEUwT-3RNGy6pZzJftFpk0iS7jM1nEU&usqp=CAU"
-            alt="M-image" class="img">
-        <ul type="none"><br>
-            <li class="li">Name ==> <strong class="strong">MEGHANA MA'AM</strong></li>
-            <li class="li">UID ==> <strong class="strong">20BCS1443</strong></li>
-            <li class="li">Section/Group ==> <strong class="strong">20ITA-19</strong></li>
-        </ul>
+    <div class="column3">
+    <img src="japmeet.jpeg" class="japmeet">
+      <p >Hi, I am Japmeet Singh.I love developing websites and through this quiz project I got to learn a lot of things starting from SQL databases to frontend designing.</p>
+      <h1 class="subheading">Contribution:</h1>
+      <p>Made the login and Register flow of the website using PHP and sql database. Also helped the team in designing the pages.</p>
+        
     </div>
     </div>
-    <h2 class="h2">WORK DONE BY:</h2>
-    <ul type="none" class="work">
-        <li class="li">Nitin ==> <strong class="strong">   About_Us Page. </strong></li>
-        <li class="li">Japmeet ==> <strong class="strong">  Home Page, Register Page and Backend. </strong></li>
-        <li class="li">Meghana ==> <strong class="strong">   Quiz Page. </strong></li>
-    </ul>
+    
 </div>
 </header>   
   </body>
